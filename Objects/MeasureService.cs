277436e8f9c0f15eb2e5cf1ad2d1084c888f1fc2
Yuki-Komatsu1208 <alphard_perseus@yahoo.co.jp>
2025-07-11 17:37:26 +0900

    NewBlazorTest

---
+++ Objects/MeasureService.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorApp1.Objects
+{
+    public static class MeasureService
+    {
+        private static Random _random = new Random();
+
+        public static MeasureResult Measure()
+        { 
+            var result =new MeasureResult();   
+
+            result.MeasureValue = _random.NextSingle();
+            result.MeasureDate = DateTime.Now;
+
+            return result;
+        }
+    }
+
+    public class MeasureResult()
+    { 
+        public float MeasureValue { get; set; }
+        public DateTime MeasureDate { get; set; }
+    
+    
+    
+    }
+}
